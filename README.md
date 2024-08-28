c464d9efdb64413bff726f842b1a0e4fd625d971
Damian Li <ke_2021@126.com>
2024-08-28 00:08:16 -0400

    finish rbtree insertion unit test

--- README.md
+++ README.md
@@ -2,30 +2,30 @@
 Stores key-value pairs and allows retrieval of a value based on its key.
 ## API
 **Open and Run**
-```
+```c++
 Open("database name")
 ```
 **Stores a key associated with a value**
-```
+```c++
 Put(key, value)
 ```
 **Retrieves a value associated with a given key**
-```
+```c++
 Value = Get(key)
 ```
 **Retrieves all KV-pairs in a key range in key order (key1 < key2)**
-```
+```c++
 KV-pairs = Scan(Key1, Key2)
 ```
 **Close**
-```
+```c++
 Close()
 ```
 **Update**
-```
+```c++
 TBA
 ```
 **Delete**
-```
+```c++
 TBA
 ```
--- tests/redblacktree_unittest.cpp
+++ tests/redblacktree_unittest.cpp
@@ -2,6 +2,7 @@
 // Created by Damian Li on 2024-08-27.
 //
 #include <gtest/gtest.h>
+#include <string>
 #include "RedBlackTree.h"
 
 // Test case for inserting a single node
@@ -41,4 +42,116 @@ TEST(RedBlackTreeTest, InsertMultipleNodes) {
     EXPECT_TRUE(rbt->search(40));
     delete rbt;
 
+}
+
+// RBTree visualization tool: https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
+TEST(RedBlackTreeTest, InsertSimpleSequence) {
+    RedBlackTree* rbt = new RedBlackTree();
+    rbt->insert(10, 100);
+    rbt->insert(20, 200);
+    rbt->insert(30, 300);
+
+    testing::internal::CaptureStdout();
+    rbt->inorderTraversal();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    std::string expectedOutput =
+        "Key: 10, Value: 100, Color: Red\n"
+        "Key: 20, Value: 200, Color: Black\n"
+        "Key: 30, Value: 300, Color: Red\n";
+
+    EXPECT_EQ(output, expectedOutput);
+    delete rbt;
+}
+
+TEST(RedBlackTreeTest, InsertReverseSequence) {
+    RedBlackTree* rbt = new RedBlackTree();
+    rbt->insert(30, 300);
+    rbt->insert(20, 200);
+    rbt->insert(10, 100);
+
+    testing::internal::CaptureStdout();
+    rbt->inorderTraversal();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    std::string expectedOutput =
+        "Key: 10, Value: 100, Color: Red\n"
+        "Key: 20, Value: 200, Color: Black\n"
+        "Key: 30, Value: 300, Color: Red\n";
+
+    EXPECT_EQ(output, expectedOutput);
+    delete rbt;
+}
+
+TEST(RedBlackTreeTest, InsertMixedSequence) {
+    RedBlackTree* rbt = new RedBlackTree();
+    rbt->insert(20, 200);
+    rbt->insert(10, 100);
+    rbt->insert(30, 300);
+    rbt->insert(15, 150);
+
+    testing::internal::CaptureStdout();
+    rbt->inorderTraversal();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    std::string expectedOutput =
+        "Key: 10, Value: 100, Color: Black\n"
+        "Key: 15, Value: 150, Color: Red\n"
+        "Key: 20, Value: 200, Color: Black\n"
+        "Key: 30, Value: 300, Color: Black\n";
+
+    EXPECT_EQ(output, expectedOutput);
+    delete rbt;
+}
+
+TEST(RedBlackTreeTest, InsertLargerSet) {
+    RedBlackTree* rbt = new RedBlackTree();
+    rbt->insert(40, 400);
+    rbt->insert(20, 200);
+    rbt->insert(10, 100);
+    rbt->insert(30, 300);
+    rbt->insert(50, 500);
+    rbt->insert(60, 600);
+
+    testing::internal::CaptureStdout();
+    rbt->inorderTraversal();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    std::string expectedOutput =
+        "Key: 10, Value: 100, Color: Black\n"
+        "Key: 20, Value: 200, Color: Black\n"
+        "Key: 30, Value: 300, Color: Black\n"
+        "Key: 40, Value: 400, Color: Red\n"
+        "Key: 50, Value: 500, Color: Black\n"
+        "Key: 60, Value: 600, Color: Red\n";
+
+    EXPECT_EQ(output, expectedOutput);
+    delete rbt;
+}
+
+
+TEST(RedBlackTreeTest, UpdateExistedValue) {
+    RedBlackTree* rbt = new RedBlackTree();
+    rbt->insert(40, 400);
+    rbt->insert(20, 200);
+    rbt->insert(10, 100);
+    rbt->insert(30, 300);
+    rbt->insert(50, 500);
+    rbt->insert(60, 600);
+    rbt->insert(10, 900);
+
+    testing::internal::CaptureStdout();
+    rbt->inorderTraversal();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    std::string expectedOutput =
+        "Key: 10, Value: 900, Color: Black\n"
+        "Key: 20, Value: 200, Color: Black\n"
+        "Key: 30, Value: 300, Color: Black\n"
+        "Key: 40, Value: 400, Color: Red\n"
+        "Key: 50, Value: 500, Color: Black\n"
+        "Key: 60, Value: 600, Color: Red\n";
+
+    EXPECT_EQ(output, expectedOutput);
+    delete rbt;
 }
